17bec807d11dbbbc616fb78df65b4aee209a38f7
xaos10123 <xaos10123@gmail.com>
2025-01-11 23:29:27 +0700

    Создание второго приложения для КАТАЛОГА

--- goods/urls.py
+++ goods/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import index
 
-app_name = 'main'
+app_name = 'goods'
 
 urlpatterns = [
     path('', index,  name='index'),
--- ice_bear/urls.py
+++ ice_bear/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('catalog/', include('goods.urls')),
 ]
