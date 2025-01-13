f47b5be6dccf36d9d5456d9b2a2ec34f97a65d68
xaos10123 <xaos10123@gmail.com>
2025-01-13 15:40:48 +0700

    Начало перехода на HTMX

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import IndexView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index,  name='index'),
+    path('', IndexView.as_view(),  name='index'),
 ]
