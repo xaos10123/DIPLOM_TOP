82190b00c8d186dcb1cfc873c07b9c9c33b0cb79
xaos10123 <xaos10123@gmail.com>
2025-01-21 01:25:23 +0700

    Подчистка

--- users/views.py
+++ users/views.py
@@ -5,12 +5,9 @@ from django.urls import reverse_lazy
 from django.views import View
 from main.views import CustomHtmxMixin
 from users.forms import UserLoginForm, UserRegistrationForm
-from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
 from users.models import Adress
-
-
 
 
 def login_view(request):
@@ -46,7 +43,7 @@ def registration_view(request):
                     "title_block": "Регистрация прошла успешно!",
                     "link_to": reverse_lazy("goods:catalog"),
                     "link_to_text": "Перейти к покупкам",
-                }
+                },
             )
     else:
         form = UserRegistrationForm()
@@ -67,36 +64,34 @@ def logout_view(request):
 @require_POST
 @login_required
 def set_active_address(request):
-    address_id = request.POST.get('address_id')
+    address_id = request.POST.get("address_id")
     request.user.addresses.all().update(is_active=False)
     address = request.user.addresses.get(id=address_id)
     address.is_active = True
     address.save()
-    request.session['active_address_id'] = address_id
-    
-    return render(request, 'users/address_dropdown.html', {
-        'active_address': address
-    })
+    request.session["active_address_id"] = address_id
+
+    return render(request, "users/address_dropdown.html", {"active_address": address})
+
 
 @login_required
 def add_address(request):
-    if request.method == 'POST':
-        city = request.POST.get('city')
-        street = request.POST.get('street')
-        house = request.POST.get('house')
-        apartment = request.POST.get('apartment')
+    if request.method == "POST":
+        city = request.POST.get("city")
+        street = request.POST.get("street")
+        house = request.POST.get("house")
+        apartment = request.POST.get("apartment")
 
         request.user.addresses.all().update(is_active=False)
-        
+
         Adress.objects.create(
             user=request.user,
             city=city,
             street=street,
             house=house,
             apartment=apartment,
-            is_active=True
+            is_active=True,
         )
-        return redirect('main:index')
-        
+        return redirect("main:index")
 
-    return render(request, 'users/add_address.html')
+    return render(request, "users/add_address.html")
