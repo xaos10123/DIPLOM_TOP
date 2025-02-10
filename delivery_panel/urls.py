c5cadc8f1ef2f2078db64c84df9b96601e383684
xaos10123 <xaos10123@gmail.com>
2025-02-11 01:27:51 +0700

    Страница и url для доставкки

--- delivery_panel/urls.py
+++ delivery_panel/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from delivery_panel.views import list_delivery
+from delivery_panel.views import list_delivery, get_order, my_delivery
 
 app_name = "delivery"
 
 urlpatterns = [
     path("", list_delivery, name="delivery_panel"),
+    path('get_order/<int:pk>', get_order, name='get_order'),
+    path('my_delivery/', my_delivery, name='my_delivery'),
 ]
--- delivery_panel/views.py
+++ delivery_panel/views.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from orders.models import Order
 
 
 @login_required
 def list_delivery(request):
-    return render(request, 'delivery_panel/delivery_panel.html')
+    order = Order.objects.filter(is_paid=True, delivery_man=None).order_by("-id")
+    return render(request, "delivery_panel/delivery_panel.html", {"orders": order})
+
+
+@login_required
+def get_order(request, pk):
+    order = Order.objects.get(pk=pk)
+    if not order.delivery_man:
+        order.delivery_man = request.user
+        order.status = "В доставке"
+        order.save()
+    return redirect(to="delivery:my_delivery")
+
+
+@login_required
+def my_delivery(request):
+    order = Order.objects.filter(delivery_man=request.user, is_paid=True)
+    return render(request, "delivery_panel/delivery_panel.html", {"orders": order})
